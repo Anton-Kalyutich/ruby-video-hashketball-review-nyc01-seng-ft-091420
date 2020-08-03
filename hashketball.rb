4ffe54ba4e1c3a9445d8681b211bdd8517d7cf5a
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-03 01:46:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,7 +196,7 @@ def big_shoe_rebounds
   the_hash = game_hash
   the_hash.each_value do |hash|
     hash[:players].each do |player|
-      if player[:rebounds] > num
+      if player[:shoe] > num
         num = 
   
 end
